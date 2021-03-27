fc5a65d516189c560a6bb625c29a9edead3d6d70
BlueTheDuck <no-reply@perezv.ar>
2021-03-26 22:42:14 -0300

    Add all interrupts flags

--- nds-sys/src/interrupts.rs
+++ nds-sys/src/interrupts.rs
@@ -11,10 +11,34 @@ pub enum WaitFor {
     Next = 1,
 }
 
-/// Flags to mask de last interrupt
+/// Flags to mask the interrupts
 #[repr(u32)]
 pub enum Flags {
-    VBlank = 1,
-    HBlank = 2,
+    /// Vertical blank
+    VBlank = bit!(0),
+    /// Horizontal blank
+    HBlank = bit!(1),
+    /// Cout match
+    VCount = bit!(2),
+    Timer0 = bit!(3),
+    Timer1 = bit!(4),
+    Timer2 = bit!(5),
+    Timer3 = bit!(6),
+    Network = bit!(7),
+    Dma0 = bit!(8),
+    Dma1 = bit!(9),
+    Dma2 = bit!(10),
+    Dma3 = bit!(11),
+    Keys = bit!(12),
+    Cart = bit!(13),
+    IpcSync = bit!(16),
+    FifoEmpty = bit!(17),
+    FifoNotEmpty = bit!(18),
+    Card = bit!(19),
+    CardLine = bit!(20),
+    GeometryFifo = bit!(21),
+    Lid = bit!(22),
+    Spi = bit!(23),
+    Wifi = bit!(24),
+    All = bit!(25) - 1,
 }
-
