f727152e1bc26080acfe959f5399c855e663449a
BlueTheDuck <no-reply@perezv.ar>
2021-03-29 17:00:49 -0300

    Improvements on the interrupts module

--- nds-sys/src/interrupts.rs
+++ nds-sys/src/interrupts.rs
@@ -1,44 +1,38 @@
 extern "C" {
     pub fn swiWaitForVBlank();
     pub fn swiIntrWait(waitForSet: u32, flags: u32);
+    pub fn irqEnable(irq: u32);
+    pub fn irqDisable(irq: u32);
 }
 
-#[repr(u32)]
-pub enum WaitFor {
-    /// Don't wait if this interrupt has already fired
-    None = 0,
-    /// Wait for the next time the interrupt is fired
-    Next = 1,
-}
+pub static mut REG_IE: *mut u32 = 0x04000210 as *mut _;
+pub static mut REG_IME: *mut u32 = 0x04000208 as *mut _;
 
-/// Flags to mask the interrupts
-#[repr(u32)]
-pub enum Flags {
-    /// Vertical blank
-    VBlank = bit!(0),
-    /// Horizontal blank
-    HBlank = bit!(1),
-    /// Cout match
-    VCount = bit!(2),
-    Timer0 = bit!(3),
-    Timer1 = bit!(4),
-    Timer2 = bit!(5),
-    Timer3 = bit!(6),
-    Network = bit!(7),
-    Dma0 = bit!(8),
-    Dma1 = bit!(9),
-    Dma2 = bit!(10),
-    Dma3 = bit!(11),
-    Keys = bit!(12),
-    Cart = bit!(13),
-    IpcSync = bit!(16),
-    FifoEmpty = bit!(17),
-    FifoNotEmpty = bit!(18),
-    Card = bit!(19),
-    CardLine = bit!(20),
-    GeometryFifo = bit!(21),
-    Lid = bit!(22),
-    Spi = bit!(23),
-    Wifi = bit!(24),
-    All = bit!(25) - 1,
+bitflags! {
+    pub struct Flags: u32 {
+        const VBLANK = bit!(0);
+        const HBLANK = bit!(1);
+        const VCOUNT = bit!(2);
+        const TIMER0 = bit!(3);
+        const TIMER1 = bit!(4);
+        const TIMER2 = bit!(5);
+        const TIMER3 = bit!(6);
+        const NETWORK = bit!(7);
+        const DMA0 = bit!(8);
+        const DMA1 = bit!(9);
+        const DMA2 = bit!(10);
+        const DMA3 = bit!(11);
+        const KEYS = bit!(12);
+        const CART = bit!(13);
+        const IPCSYNC = bit!(16);
+        const FIFOEMPTY = bit!(17);
+        const FIFONOTEMPTY = bit!(18);
+        const CARD = bit!(19);
+        const CARDLINE = bit!(20);
+        const GEOMETRYFIFO = bit!(21);
+        const LID = bit!(22);
+        const SPI = bit!(23);
+        const WIFI = bit!(24);
+        const ALL = bit!(25) - 1;
+    }
 }
--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,16 +1,39 @@
-pub use interrupts::{Flags, WaitFor};
-use nds_sys::interrupts::{self, swiIntrWait, swiWaitForVBlank};
+pub use interrupts::Flags;
+use nds_sys::interrupts::{self, swiIntrWait, swiWaitForVBlank, irqEnable, irqDisable};
 
-/// Waits until the next VBlank
-/// (Same as `swi_intr_wait(WaitFor::Next, Flags::VBlank)`)
+pub mod registers {
+    pub use nds_sys::interrupts::{REG_IE, REG_IME};
+}
+
+/// Waits until the next VBlank.
+/// (Same as [`swi_intr_wait(Flags::VBLANK, true)`](swi_intr_wait).
 pub fn swi_wait_for_v_blank() {
     unsafe {
         swiWaitForVBlank();
     }
 }
 
-pub fn swi_intr_wait(wait_for: WaitFor, flags: Flags) {
+/// Waits until one of the interrupts specified in `flags` is fired.
+/// If that interrupt happened, but hasn't been processed yet, then
+/// `swi_intr_wait` returns immediatly only if `wait_for_next` is `false`.
+pub fn swi_intr_wait(flags: Flags, wait_for_next: bool) {
     unsafe {
-        swiIntrWait(wait_for as u32, flags as u32);
+        swiIntrWait(if wait_for_next { 1 } else { 0 }, flags.bits());
     }
+}
+
+/// Enable the interrupts specified in `irq`.
+/// OR different flags to enable many interrupts at once.
+/// This function is unsafe since changing the state of interrupts
+/// can (will!) break other code (for example: [`wait_for`](crate::dma::wait_for))
+pub unsafe fn irq_enable(irq: Flags) {
+    irqEnable(irq.bits());
+}
+
+/// Disable the interrupts specified in `irq`.
+/// OR different flags to disable many interrupts at once.
+/// This function is unsafe since changing the state of interrupts
+/// can (will!) break other code (for example: [`wait_for`](crate::dma::wait_for))
+pub unsafe fn irq_disable(irq: Flags) {
+    irqDisable(irq.bits());
 }
