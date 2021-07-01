e1e5a8b958c1595b9ac5c28793797ede0d4b2df5
BlueTheDuck <hello@perezv.ar>
2021-07-01 03:42:05 -0300

    Change `static`->`const` and move mask out of bitflags

--- nds-sys/src/video.rs
+++ nds-sys/src/video.rs
@@ -1,55 +1,53 @@
 //! Video related functions and constants.
 //! Mostly "borrowed" from libnds's video.h
 
-pub const BG_GFX: *mut u16 = 0x06000000 as *mut u16;
-pub const BG_GFX_SUB: *mut u16 = 0x06200000 as *mut u16;
+pub const BG_GFX: *mut u16 = 0x06000000 as _;
+pub const BG_GFX_SUB: *mut u16 = 0x06200000 as _;
 
 /// Display control register (main). Used to choose the mode for the Main Engine
-pub static mut REG_DISPCNT: *mut u32 = 0x04000000 as *mut u32;
+pub const REG_DISPCNT: *mut u32 = 0x04000000 as _;
 /// Display control register (sub). Used to choose the mode for the Sub Engine
-pub static mut REG_DISPCNT_SUB: *mut u32 = 0x04001000 as *mut u32;
+pub const REG_DISPCNT_SUB: *mut u32 = 0x04001000 as _;
 
-pub static VRAM_ENABLE: u8 = 1 << 7;
+pub const VRAM_ENABLE: u8 = 1 << 7;
 
 /// Control register for VRAM A
-pub static mut VRAM_A_CR: *mut u8 = 0x04000240 as *mut u8;
+pub const VRAM_A_CR: *mut u8 = 0x04000240 as _;
 /// Control register for VRAM B
-pub static mut VRAM_B_CR: *mut u8 = 0x04000241 as *mut u8;
+pub const VRAM_B_CR: *mut u8 = 0x04000241 as _;
 /// Control register for VRAM C
-pub static mut VRAM_C_CR: *mut u8 = 0x04000242 as *mut u8;
+pub const VRAM_C_CR: *mut u8 = 0x04000242 as _;
 /// Control register for VRAM D
-pub static mut VRAM_D_CR: *mut u8 = 0x04000243 as *mut u8;
+pub const VRAM_D_CR: *mut u8 = 0x04000243 as _;
 /// Control register for VRAM E
-pub static mut VRAM_E_CR: *mut u8 = 0x04000244 as *mut u8;
+pub const VRAM_E_CR: *mut u8 = 0x04000244 as _;
 /// Control register for VRAM F
-pub static mut VRAM_F_CR: *mut u8 = 0x04000245 as *mut u8;
+pub const VRAM_F_CR: *mut u8 = 0x04000245 as _;
 /// Control register for VRAM G
-pub static mut VRAM_G_CR: *mut u8 = 0x04000246 as *mut u8;
+pub const VRAM_G_CR: *mut u8 = 0x04000246 as _;
 /// Control register for VRAM H
-pub static mut VRAM_H_CR: *mut u8 = 0x04000248 as *mut u8;
+pub const VRAM_H_CR: *mut u8 = 0x04000248 as _;
 /// Control register for VRAM I
-pub static mut VRAM_I_CR: *mut u8 = 0x04000249 as *mut u8;
+pub const VRAM_I_CR: *mut u8 = 0x04000249 as _;
 
 /// When VRAM A is in LCD mode, this points to the first pixel (top left most)
-pub static mut VRAM_A: *mut u16 = 0x6800000 as *mut u16;
+pub const VRAM_A: *mut u16 = 0x6800000 as _;
 /// When VRAM B is in LCD mode, this points to the first pixel (top left most)
-pub static mut VRAM_B: *mut u16 = 0x6820000 as *mut u16;
+pub const VRAM_B: *mut u16 = 0x6820000 as _;
 /// When VRAM C is in LCD mode, this points to the first pixel (top left most)
-pub static mut VRAM_C: *mut u16 = 0x6840000 as *mut u16;
+pub const VRAM_C: *mut u16 = 0x6840000 as _;
 /// When VRAM D is in LCD mode, this points to the first pixel (top left most)
-pub static mut VRAM_D: *mut u16 = 0x6860000 as *mut u16;
+pub const VRAM_D: *mut u16 = 0x6860000 as _;
 
 /// Background palette (Main)
-pub static mut BG_PALETTE: *mut u16 = 0x05000000 as *mut u16;
+pub const BG_PALETTE: *mut u16 = 0x05000000 as _;
 /// Background palette (Sub)
-pub static mut BG_PALETTE_SUB: *mut u16 = 0x05000400 as *mut u16;
+pub const BG_PALETTE_SUB: *mut u16 = 0x05000400 as _;
 
 bitflags! {
     pub struct Flags: u32 {
         /// Use extended palette
         const EXT_PALETTE = bit!(30);
-        /// These bits multiplied by 0x100000 gives the base offset for the background data
-        const BG_SCREEN_BASE_MASK = 3 << 27;
         // These are mutually exclusive
         /// When [DISPLAY_VRAM] is set, display from this bank
         const VRAM_D = 3 << 18;
@@ -96,3 +94,8 @@ bitflags! {
         const MODE0 = 0;
     }
 }
+
+pub const MAP_BASE_OFFSET: u32 = 27;
+pub const TILES_BASE_OFFSET: u32 = 24;
+pub const MAP_BASE_MASK: u32 = 3 << 27;
+pub const TILES_BASE_MASK: u32 = 3 << 24;
