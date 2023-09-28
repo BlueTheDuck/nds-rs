367e5d7e32c48076057243bf5d98bb00dc55fe6f
BlueTheDuck <hello@perezv.ar>
2023-09-28 00:17:26 -0300

    Simplify entry macro

--- nds-proc-macros/src/lib.rs
+++ nds-proc-macros/src/lib.rs
@@ -22,35 +22,13 @@ use syn::{parse_macro_input, ItemFn};
 pub fn entry(_: TokenStream, input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as ItemFn);
     let name = input.sig.ident.clone();
-    let returns;
-    if let Some(ret) = input.sig.output.next() {
-        match ret {
-            syn::ReturnType::Default => returns = true,
-            syn::ReturnType::Type(_, ref ty) => match **ty {
-                syn::Type::Never(_) => returns = false,
-                _ => returns = true,
-            },
-        }
-    } else {
-        returns = false;
-    }
-
-    if returns {
-        quote! {
-            #[no_mangle]
-            pub extern "C" fn main() -> ! {
-                #input
-                let ret = #name();
-                panic!("main() returned {:?}",ret);
-            }
-        }
-    } else {
-        quote! {
-            #[no_mangle]
-            pub extern "C" fn main() -> ! {
-                #input
-                #name();
-            }
+    
+    quote! {
+        #[no_mangle]
+        pub extern "C" fn main() -> ! {
+            #input
+            let ret = #name();
+            panic!("main() returned {:?}",ret);
         }
     }
     .into()
