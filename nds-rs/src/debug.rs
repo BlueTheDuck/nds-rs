48c110b530fbc9a555c4a404b8df74c809d1b837
BlueTheDuck <hello@perezv.ar>
2024-12-06 02:39:00 -0300

    Improve debug module
    Supports writing to either output device (tty or no$gba) using standard `write!` macro.
    No need to carry structs, `Tty` and `NoCash` are zero-sized and always constructable
    Output strings in null-terminated chunks while keeping %fmt% intact.

--- nds-rs/src/debug.rs
+++ nds-rs/src/debug.rs
@@ -1,4 +1,7 @@
-use core::fmt::{Arguments, Write};
+use core::{
+    ffi::CStr,
+    fmt::{Arguments, Write},
+};
 use nds_sys::debug::registers;
 use spin::Mutex;
 extern crate alloc;
@@ -20,14 +23,17 @@ macro_rules! println {
 #[derive(Clone, Copy)]
 enum Logger {
     None,
-    NoCash(NoCash),
-    Tty(Tty),
+    NoCash,
+    Tty,
 }
+
+/// This is the logging target that will be used by the `print!` and `println!` macros.
+static LOGGER: Mutex<Logger> = Mutex::new(Logger::None);
 
 pub fn log_to_nocash() -> bool {
     let mut logger_lock = LOGGER.lock();
-    if let Some(nocash) = NoCash::new() {
-        *logger_lock = Logger::NoCash(nocash);
+    if NoCash::get_emu_id().is_some() {
+        *logger_lock = Logger::NoCash;
         true
     } else {
         false
@@ -44,7 +50,7 @@ pub fn log_to_tty() -> bool {
     unsafe {
         console_demo_init();
     }
-    *logger_lock = Logger::Tty(Tty::new());
+    *logger_lock = Logger::Tty;
 
     true
 }
@@ -58,33 +64,31 @@ pub fn _print(args: Arguments) {
     let logger = LOGGER.lock();
 
     // Check fast case: the string is null-terminated and is one segment
-    if let Some(cstr) = args.as_str().filter(|s| s.ends_with('\0')) {
+    if let Some(cstr) = args
+        .as_str()
+        .and_then(|s| CStr::from_bytes_with_nul(s.as_bytes()).ok())
+    {
         match *logger {
-            Logger::NoCash(mut nocash) => {
-                nocash.write_str(cstr).unwrap();
+            Logger::NoCash => {
+                NoCash.write_cstr_param(cstr);
             }
-            Logger::Tty(mut tty) => {
-                tty.write_str(cstr).unwrap();
+            Logger::Tty => {
+                Tty.write_cstr(cstr);
             }
             _ => {}
         }
     } else {
         match *logger {
-            Logger::NoCash(mut nocash) => {
-                write!(nocash, "{args}").unwrap();
+            Logger::NoCash => {
+                write!(NoCash, "{args}").unwrap();
             }
-            Logger::Tty(mut tty) => {
-                write!(tty, "{args}").unwrap();
+            Logger::Tty => {
+                write!(Tty, "{args}").unwrap();
             }
             _ => {}
         }
     }
 }
-
-/// Used to access all NO$GBA debugging functions.
-///
-/// NoCash is detected at startup, while TTY may be enabled or disabled at any time.
-static LOGGER: Mutex<Logger> = Mutex::new(Logger::None);
 
 /// Symbolizes the emulator NO$GBA and provides
 /// an API for its debugging capabilities.
@@ -92,15 +96,11 @@ static LOGGER: Mutex<Logger> = Mutex::new(Logger::None);
 #[derive(Clone, Copy)]
 pub struct NoCash;
 impl NoCash {
-    pub fn new() -> Option<Self> {
-        match Self::get_emu_id() {
-            Some("NO$GBA") => Some(Self),
-            Some(s) if s.starts_with("melonDS") => Some(Self),
-            _ => None,
-        }
-    }
+    /// The maximum size of the buffer used to write to the emulator TTY.
+    /// Buffers are used to null-terminate strings before passing them to the emulator.
+    const BUFSIZE: usize = 256;
 
-    /// Prints to the emulator TTY. NO$GBA will substitute %fmt% with a value
+    /// Writes to the emulator TTY. NO$GBA will substitute %fmt% with a value
     /// according to this list (taken from [gbatek]):
     ///
     /// - r0,r1,r2,...,r15: show register content (displayed as 32bit Hex number)
@@ -111,17 +111,16 @@ impl NoCash {
     /// - lastclks: show number of cycles since previous lastclks (or zeroclks)
     /// - zeroclks: resets the 'lastclks' counter
     ///
-    /// # Safety
-    /// The caller must make sure the string is null-terminated
-    ///
     /// [gbatek]: http://problemkaputt.de/gbatek.htm#debugmessages
     #[inline]
-    pub unsafe fn print_with_params_no_alloc(&mut self, s: &str) {
-        registers::STRING_OUT_PARAM_LF.write_volatile(s.as_ptr());
+    pub fn write_cstr_param(&mut self, s: &CStr) {
+        unsafe {
+            registers::STRING_OUT_PARAM.write_volatile(s.as_ptr() as _);
+        }
     }
 
-    /// Prints to the emulator TTY. NO$GBA will substitute %fmt% with a value
-    /// according to this list (taken from [gbatek]):
+    /// Formats the arguments and writes the resulting string to the emulator TTY. NO$GBA
+    /// will substitute %fmt% with a value according to this list (taken from [gbatek]):
     ///
     /// - r0,r1,r2,...,r15: show register content (displayed as 32bit Hex number)
     /// - sp,lr,pc: alias for r13,r14,r15
@@ -133,23 +132,29 @@ impl NoCash {
     ///
     ///  [gbatek]: http://problemkaputt.de/gbatek.htm#debugmessages
     #[inline]
-    pub fn print_with_params(&mut self, s: &Arguments) {
+    pub fn write_fmt_with_params(&mut self, s: &Arguments) {
         let formatted = alloc::format!("{s}\0");
-        unsafe {
-            registers::STRING_OUT_PARAM_LF.write_volatile(formatted.as_ptr());
+        write!(self, "{formatted}").unwrap();
+    }
+
+    /// Helper function to write a string in chunks to the emulator TTY.
+    ///
+    /// The input is copied to a buffer and null-terminated, then passed to the emulator.
+    fn write_in_chunks<const B: usize>(&mut self, s: &str, buf: &mut [u8; B]) {
+        for chunk in s.as_bytes().chunks(B - 1) {
+            buf[..chunk.len()].copy_from_slice(chunk);
+            buf[chunk.len()] = 0;
+            // SAFETY: ^ we just null-terminated the buf
+            let cstr = unsafe { CStr::from_bytes_with_nul_unchecked(buf) };
+            self.write_cstr_param(cstr);
         }
     }
 
     /// Returns a string identifying the emulator that the ROM is running on.
     /// Only NO$GBA supports this, so don't rely on it
     pub fn get_emu_id() -> Option<&'static str> {
-        if cfg!(debug_assertions) {
-            let emu_id = unsafe { core::slice::from_raw_parts(registers::EMU_ID_PTR, 16) };
-            core::str::from_utf8(emu_id).ok()
-        } else {
-            // Don't give access to debug stuff on release
-            None
-        }
+        let emu_id = unsafe { core::slice::from_raw_parts(registers::EMU_ID_PTR, 16) };
+        core::str::from_utf8(emu_id).ok()
     }
 }
 impl Write for NoCash {
@@ -157,15 +162,47 @@ impl Write for NoCash {
         // NO$GBA uses C-style strings, so we can only pass null-terminated strings.
         // Since we promised to *not* allocate, we can only use this register
         // **if, and only if,** the string we received is already null-terminated...
-        if s.ends_with('\0') {
-            unsafe { registers::STRING_OUT_PARAM.write_volatile(s.as_ptr()) }
+        if let Ok(s) = CStr::from_bytes_until_nul(s.as_bytes()) {
+            self.write_cstr_param(s);
         } else {
-            // ...otherwise we have to print it char by char
-            for c in s.chars() {
-                unsafe {
-                    registers::CHAR_OUT.write_volatile(c);
-                }
+            // ...otherwise we have to copy it to a buffer and null-terminate it.
+            // Since we have a fixed-size buffer, we have to be careful when coping
+            // chunks of the string to it, we mustn't break the %fmt% specifiers.
+
+            let mut buffer = [0; NoCash::BUFSIZE];
+
+            let mut remaining = s;
+            let mut next_marker = remaining.find("%");
+            while let Some(opening_marker_pos) = next_marker {
+                let closing_marker_pos = remaining[opening_marker_pos + 1..]
+                    .find("%")
+                    .map(|pos| pos + 1)
+                    .unwrap_or(remaining.len());
+
+                // if the closing marker is close enought
+                // we can include the entire specifier
+                let split_point = if closing_marker_pos < Self::BUFSIZE - 1 {
+                    closing_marker_pos
+                } else {
+                    // otherwise we stop just before the spec
+                    // NOTE:
+                    //  1. openin_marker_pos == 0 -> openin_marker_pos-1 -> panic!
+                    //  2. openin_marker_pos > 0 -> openin_marker_pos-1 -> valid
+                    // Since `closing_marker_pos = opening_marker_pos + n` and `n < Self::BUFSIZE`
+                    // making `closing_marker_pos < Self::BUFSIZE` always true or #1 always false
+                    opening_marker_pos.saturating_sub(1)
+                };
+                let Some((to_write, rest)) = remaining.split_at_checked(split_point) else {
+                    // split_point comes from `find`, so it's always valid
+                    unreachable!();
+                };
+                remaining = rest;
+                next_marker = remaining.find("%");
+                self.write_in_chunks(to_write, &mut buffer);
             }
+
+            // write the remaining part of the string
+            self.write_in_chunks(remaining, &mut buffer);
         }
         Ok(())
     }
@@ -179,16 +216,15 @@ impl Write for NoCash {
 }
 
 #[derive(Clone, Copy)]
-struct Tty;
+pub struct Tty;
 impl Tty {
-    pub fn new() -> Self {
-        Self
+    pub fn write_cstr(&mut self, cstr: &CStr) {
+        picolibc::printf(cstr);
     }
 }
-
 impl Write for Tty {
     fn write_str(&mut self, s: &str) -> core::fmt::Result {
-        picolibc::write_str_to_stderr(s);
+        picolibc::fwrite(s.as_bytes(), picolibc::STDOUT);
 
         Ok(())
     }
