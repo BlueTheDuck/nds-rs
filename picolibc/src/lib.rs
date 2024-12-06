f92a20da9c42799a5101eb6a8180d9a7fd72b324
BlueTheDuck <hello@perezv.ar>
2024-12-06 02:35:31 -0300

    add safe bindings to the print and write C functions

--- picolibc/src/lib.rs
+++ picolibc/src/lib.rs
@@ -1,15 +1,28 @@
 #![no_std]
 
+use core::{ffi::CStr, ptr::NonNull};
+
+use bindings::FILE;
+
+unsafe extern "C" {
+    #[link_name = "stdin"]
+    pub safe static STDIN: NonNull<FILE>;
+    #[link_name = "stdout"]
+    pub safe static STDOUT: NonNull<FILE>;
+    #[link_name = "stderr"]
+    pub safe static STDERR: NonNull<FILE>;
+}
+
 pub mod bindings {
     #![allow(warnings)]
-    
     include!(concat!(env!("OUT_DIR"), "/picolibc.rs"));
 }
 
 #[inline]
-pub fn write_str_to_stderr(s: &str) -> usize {
-    let buffer = s.as_bytes();
-    unsafe {
-        bindings::fwrite(buffer.as_ptr() as _, buffer.len() as _, 1, bindings::stderr) as _
-    }
+pub fn printf(s: &CStr) -> usize {
+    unsafe { bindings::printf(s.as_ptr() as _) as _ }
+}
+
+pub fn fwrite(s: &[u8], stream: NonNull<FILE>) -> usize {
+    unsafe { bindings::fwrite(s.as_ptr() as _, s.len() as _, 1, stream.as_ptr()) as _ }
 }
